d3f8afa0446d42bef745c8b6200ac5c187345e98
Conor Baumgart & Tom Mertz <student@epicodus.com>
2015-03-10 16:32:47 -0700

    Add twig to display results

--- app/app.php
+++ app/app.php
@@ -5,8 +5,23 @@
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        return "hay hay hay";
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__."/../views"));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('form.twig');
+    });
+
+    $app->post("/bananagram", function() use ($app) {
+
+        $input_of_words = explode(" ", $_POST['list_of_words']);
+        $input = $_POST['single_word'];
+
+        $anagram = new Anagram();
+
+        $result_array = $anagram->findAnagram($input, $input_of_words);
+
+        return $app['twig']->render('result.twig', array('input_word' => $input, 'results' => $result_array));
+
     });
 
 
