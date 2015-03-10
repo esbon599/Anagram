e9ddd0052946b3a5b6c39d7ad1259d2c41d1aaf0
Conor Baumgart & Tom Mertz <student@epicodus.com>
2015-03-10 10:02:44 -0700

    Create function that matches single letter.

--- src/Anagram.php
+++ src/Anagram.php
@@ -4,7 +4,13 @@
     {
         function findAnagram($input, $list_of_words)
         {
-            
+            $matched_array = array();
+            foreach ($list_of_words as $word) {
+                if ($word == $input) {
+                    array_push($matched_array, $word);
+                }
+            }
+            return $matched_array;
         }
     }
 
